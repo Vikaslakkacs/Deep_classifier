962daf658babfa588768e7c394ce8382582367a8
Vikas Lakka <vikaslakkacs@gmail.com>
2022-11-21 23:38:41 +0530

    added workflow and tested using tox

--- README.md
+++ README.md
@@ -10,8 +10,10 @@
 6. Update components in src/deepClassifier/components
 7. update pipeline in src/deepClassifier/pipeline
 8. Test run pipeline stage
-9. Run tox for testing package
-10. update dvc.yaml
-11. run "dvc repro" command(reproduce) for running all stages in pipeline
+9. Run tox for testing package (simply type tox)
+10. If want to run next time use tox --recreate to recreate or you can just run tox to run on existing python version
+11. update dvc.yaml
+12. run "dvc repro" command(reproduce) for running all stages in pipeline
+13. Add github workflows to type check on github repo actions
 
 ![img](https://raw.githubusercontent.com/Vikaslakkacs/Deep_classifier/4a93274b8f2140c657e37646c6ec747c9d975451/docs/images/Data%20Ingestion%402x%20(1).png)
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,13 +3,23 @@ from deepClassifier.utils import read_yaml
 from pathlib import Path
 import os
 from box import ConfigBox
+from ensure.main import EnsureError
 
-##Dummy files for testing
-yaml_files=[
-    "testsdata/empty.yaml",
-    "testsdata/demo.yaml"
-]
 
-def test_read_yaml_empty():
-    with pytest.raises(ValueError):
-        read_yaml(Path(yaml_files[0]))
+class Test_read_yaml:
+    ##Dummy files for testing
+    yaml_files = ["tests/data/empty.yaml", "tests/data/demo.yaml"]
+
+    def test_read_yaml_empty(self):
+        with pytest.raises(ValueError):
+            read_yaml(Path(self.yaml_files[0]))
+
+    def test_read_yaml_return_type(self):
+        response = read_yaml(Path(self.yaml_files[-1]))
+        assert isinstance(response, ConfigBox)
+
+    ## If we want to send all the files present in the list we use pytest decorator
+    @pytest.mark.parametrize("path_to_yaml", yaml_files)
+    def test_read_yaml_bad_type(self, path_to_yaml):
+        with pytest.raises(EnsureError):
+            read_yaml(path_to_yaml)
